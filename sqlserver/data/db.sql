0ddacc3363fce925a88b5ec026151f02e5731f84
David Keller <dnkeller@oakland.edu>
2016-02-14 12:49:49 -0500

    Added refresh script

--- sqlserver/data/db.sql
+++ sqlserver/data/db.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE IF NOT EXISTS `USERS2` (
   `USER_UID` varchar(32) NOT NULL,
   `USER_FNAME` varchar(32) NOT NULL,
@@ -15,3 +13,13 @@ CREATE TABLE IF NOT EXISTS `USERS2` (
 
 INSERT INTO `USERS2` (`USER_UID`, `USER_FNAME`, `USER_LNAME`, `USER_EMAIL`, `USER_PWDHSH`, `USER_PWDSALT`) VALUES
 ('snolton', 'Stuart', 'Nolton', 'scnolton@oakland.edu', 'Ë®VmIlRHDEJlo', 'Ë®1=ó²Ù‘ðá}êýwð!NU·´a½ËÔ«€Á_ÿ>eçVw>¥—Zú(fAaI‰-°…€©ŸƒÐ”#‰*s');
+
+CREATE TABLE IF NOT EXISTS `BUSERS2` (
+  `USER_UID` varchar(32) NOT NULL,
+  `USER_FNAME` varchar(32) NOT NULL,
+  `USER_LNAME` varchar(32) NOT NULL,
+  `USER_EMAIL` varchar(128) NOT NULL,
+  `USER_PWDHSH` varchar(128) NOT NULL,
+  `USER_PWDSALT` varchar(128) NOT NULL,
+  PRIMARY KEY (`USER_UID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- webserver/src/index.php
+++ webserver/src/index.php
@@ -7,4 +7,6 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 echo $mysqli->host_info . "\n";
+$res = $mysqli->query("SHOW TABLES;");
+echo $res->num_rows;
 ?>
