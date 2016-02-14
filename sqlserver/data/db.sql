2a5129265509425ae5f078270ce5837b728312e9
David Keller <dnkeller@oakland.edu>
2016-02-14 03:21:32 -0500

    Working sql import

---
+++ sqlserver/data/db.sql
@@ -0,0 +1,17 @@
+
+
+CREATE TABLE IF NOT EXISTS `USERS2` (
+  `USER_UID` varchar(32) NOT NULL,
+  `USER_FNAME` varchar(32) NOT NULL,
+  `USER_LNAME` varchar(32) NOT NULL,
+  `USER_EMAIL` varchar(128) NOT NULL,
+  `USER_PWDHSH` varchar(128) NOT NULL,
+  `USER_PWDSALT` varchar(128) NOT NULL,
+  PRIMARY KEY (`USER_UID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+INSERT INTO `USERS2` (`USER_UID`, `USER_FNAME`, `USER_LNAME`, `USER_EMAIL`, `USER_PWDHSH`, `USER_PWDSALT`) VALUES
+('snolton', 'Stuart', 'Nolton', 'scnolton@oakland.edu', 'Ë®VmIlRHDEJlo', 'Ë®1=ó²Ù‘ðá}êýwð!NU·´a½ËÔ«€Á_ÿ>eçVw>¥—Zú(fAaI‰-°…€©ŸƒÐ”#‰*s');
--- webserver/src/index.php
+++ webserver/src/index.php
@@ -1,1 +1,10 @@
-<?php echo "Hello Jimbolinotonobono" ?>
+<?php
+echo "Hello frog";
+$host = getenv("DB_PORT_3306_TCP_ADDR");
+echo $host;
+$mysqli = new mysqli($host, "root", "my-secret", "morty");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+?>
