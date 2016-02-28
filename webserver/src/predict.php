4602ce31ffe2a594b0100ba60057d3a5e13c2a5b
David Keller <dnkeller@oakland.edu>
2016-02-28 16:28:02 -0500

    prediction returns prediction info. fname and lname broken

--- webserver/src/predict.php
+++ webserver/src/predict.php
@@ -3,7 +3,14 @@ require "index.php";
 
 
 function predict($file,$user) {
-	 return exec("/var/www/facerec/faces predict ".$file." ".$user);
+	 $guess = exec("/var/www/facerec/faces predict ".$file." ".$user);
+	 //echo $guess;
+	 $conn = connectToDB();
+	 $sql = "SELECT ACQUAINTANCE_FNAME, ACQUAINTANCE_LNAME, RELATION, DESCRIPTION FROM RELATIONSHIP JOIN ACQUAINTANCE WHERE USER_UID = '".$user."' AND REL_ID=".$guess.";";
+	 //echo "\n".$sql;
+	 $result=$conn->query($sql);
+	 $row = $result->fetch_assoc();
+	 print_r($row);
 }
 $filep = $_FILES['pic']['tmp_name'];
 $userid = $_POST['USERNAME'];
