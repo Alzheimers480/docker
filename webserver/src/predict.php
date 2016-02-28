e842194f5d603c61f0e26e21f8d4de34c56e9f49
David Keller <dnkeller@oakland.edu>
2016-02-28 16:37:38 -0500

    Predict seems to be working for single users! Multi-user untested

--- webserver/src/predict.php
+++ webserver/src/predict.php
@@ -6,7 +6,7 @@ function predict($file,$user) {
 	 $guess = exec("/var/www/facerec/faces predict ".$file." ".$user);
 	 //echo $guess;
 	 $conn = connectToDB();
-	 $sql = "SELECT ACQUAINTANCE_FNAME, ACQUAINTANCE_LNAME, RELATION, DESCRIPTION FROM RELATIONSHIP JOIN ACQUAINTANCE WHERE USER_UID = '".$user."' AND REL_ID=".$guess.";";
+	 $sql = "SELECT ACQUAINTANCE_FNAME, ACQUAINTANCE_LNAME, RELATION, DESCRIPTION FROM RELATIONSHIP NATURAL JOIN ACQUAINTANCE WHERE USER_UID = '".$user."' AND REL_ID=".$guess.";";
 	 //echo "\n".$sql;
 	 $result=$conn->query($sql);
 	 $row = $result->fetch_assoc();
