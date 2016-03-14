7ca431212d31eeaaa3da2cf2c61d06d0e743272f
David Keller <dnkeller@oakland.edu>
2016-03-14 00:14:00 -0400

    got rid of filltables.sh

--- fillTables.sh
+++
@@ -1,3 +0,0 @@
-curl -L $(docker-machine ip cv)/newuser.php --data "USERNAME=switch202&PASSWORD=password&PASSWORD2=password&FNAME=Grumpy&LNAME=OldFart&EMAIL=scnolton@oakland.edu";
-curl -L $(docker-machine ip cv)/newacqu.php --data "USERNAME=loopy&FNAME=Grumpy&LNAME=OldFart";
-curl -L $(docker-machine ip cv)/relate.php --data "USERNAME=switch202&ACQUNAME=loopy&RELATION=brother&MESSAGE=my older brother. he is so cool.";
